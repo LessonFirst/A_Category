2b1ba66a689e7d081838525f3b5a018d5c848944
李浩 <hao1.li@net263.com>
2019-03-25 16:40:17 +0800

    2

---
+++ A_Category/A_Category/CTMediator+A.h
@@ -0,0 +1,19 @@
+//
+//  CTMediator+A.h
+//  A_Category
+//
+//  Created by only on 2019/3/25.
+//  Copyright © 2019年 only. All rights reserved.
+//
+
+#import <CTMediator/CTMediator.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CTMediator (A)
+
+- (UIViewController *)A_aViewController;
+
+@end
+
+NS_ASSUME_NONNULL_END
